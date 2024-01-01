6efea4b7548a32b58cabcfdcab107fa2ff078caa
Giovanni Vargas <egiovanni.vo@gmail.com>
2024-01-01 13:56:59 -0600

    Link added from root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex flex-col min-h-screen items-center justify-center">
       <h1 className="text-4xl">Home Page</h1>
+      <Link
+        href={'date'}
+        className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+      >
+        <p className="hidden md:block">Dates Tools</p>
+      </Link>
     </main>
   )
 }
